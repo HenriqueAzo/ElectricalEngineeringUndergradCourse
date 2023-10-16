798881be5c9400a604073624fcbfe4a2d82ce5dd
Henrique Azoubel <84883028+HenriqueAzo@users.noreply.github.com>
2023-10-16 20:27:11 -0300

    Update Monofásico.m

--- CálculoFatordePotência/Monofásico.m
+++ CálculoFatordePotência/Monofásico.m
@@ -1,1 +1,24 @@
-#código para cálculo dos componentes de um sistema monofásico
+#Cálculo de componentes monofásicos
+
+#{
+Entradas:
+- Tensão monofásica
+- Carga (número complexo)
+
+Saídas (na ordem):
+- Fasor de corrente
+- Potências atuais:
+    Ativa
+    Reativa
+    Aparente
+- Atual Fator de Potência
+- Capacitor necessário para corrigir o FP para 0.92
+- Nova corrente e novas potências
+- Capacitor necessário para corrigir o FP para 0.96
+- Nova corrente e novas potências
+- Capacitor necessário para corrigir o FP para 1.00
+- Nova corrente e novas potências
+#}
+
+V=input("insira o valor da tensão monofásica (forma retangular): ")
+Z=input("insita o valor da carga (forma retangular): ")
