07f098d2657c918629ef955059dfe2b5ef150f61
Henrique Azoubel <84883028+HenriqueAzo@users.noreply.github.com>
2023-10-30 08:06:01 -0300

    Update Monofásico.m

--- CálculoFatordePotência/Monofásico.m
+++ CálculoFatordePotência/Monofásico.m
@@ -1,5 +1,7 @@
 #Cálculo de componentes monofásicos
-
+clear all;
+close all;
+clc;
 #{
 Entradas:
 - Tensão monofásica
@@ -12,26 +14,40 @@ Saídas (na ordem):
     Reativa
     Aparente
 - Atual Fator de Potência
-- Capacitor necessário para corrigir o FP para 0.92
+- Capacitor necessário para corrigir o FP para 0.92 (theta=23.074)
 - Nova corrente e novas potências
-- Capacitor necessário para corrigir o FP para 0.96
+- Capacitor necessário para corrigir o FP para 0.96 (theta=16.26)
 - Nova corrente e novas potências
-- Capacitor necessário para corrigir o FP para 1.00
+- Capacitor necessário para corrigir o FP para 1.00 (theta=0)
 - Nova corrente e novas potências
 #}
 
-V=input("insira o valor da tensão monofásica (forma retangular): ")
-Z=input("insita o valor da carga (forma retangular): ")
+V=input("insira o valor da tensão monofásica: ");
+Z=input("\n insira o valor da carga (forma retangular): ");
 
+if(real(Z)==0)
+  fprintf("\n\nO circuito consiste apenas de cargas reativas. \nO fator de potencia permanece zero, independente de correcoes com outras cargas reativas.\n\n")
+  return;
+  endif
+#
+#definindo corrente, w
+fprintf("\nCorrente inicial: ");
+I=V/Z
 w=2*pi*60;
 
-#definindo corrente, theta inicial e FP inicial
-I=V/Z
-conjI=conj(I)
-theta=atan(imag(I)/real(I))
-FP=cos(theta)
-
 #definir potência inicial
+fprintf("\nPotência complexa inicial: ");
 S=(V*conj(I))/2
 
-#corrigindo FP para 0.92
+#definir FP inicial
+fprintf("\nFator de potencia atual: ");
+FP=cos(atan(imag(S)/real(S)))
+FPDesejado=input("\nPara que que Fator de potencia se deseja corrigir o circuito atual?\n")
+
+while(FPDesejado>1 || FPDesejado<0)
+  FPDesejado=input("\n Valor invalido. insira um valor tal que 0<=FP<=1.")
+endwhile
+#
+#fórmula para FP=0.92
+fprintf("\n\nCalculando C: ");
+C=(real(S)/((V.^2)*w))*(tan(acos(FP))-tan(acos(FPDesejado)))
